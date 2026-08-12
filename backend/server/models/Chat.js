a81f80975ae60c92278d3aeecf53b17018c2bc55
Minyamir Kelemu <minyamirkelemu12@gmail.com>
2026-08-12 09:15:20 +0300

    Update Chat.js

--- backend/server/models/Chat.js
+++ backend/server/models/Chat.js
@@ -1,12 +1,3 @@
 import mongoose from "mongoose";
 
-const chatSchema = new mongoose.Schema({
-  userText: String,
-  aiText: String,
-  language: String,
-  image: String,
-  audio: String,
-  createdAt: { type: Date, default: Date.now }
-});
 
-export default mongoose.model("Chat", chatSchema);
